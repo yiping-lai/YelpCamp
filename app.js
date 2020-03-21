7f4632f4e6c23ae4472db196888fa62feba02907
YiPing <monoyp0211@gmail.com>
2020-03-21 23:29:30 +0000

    update listen to process.env

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(3000, function() { 
+//app.listen(3000, function() { 
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log('Server listening on port 3000'); 
 });
